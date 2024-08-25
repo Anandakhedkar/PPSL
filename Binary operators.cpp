7bae8d949e6f8814a49cf391795bf4148e3dd90d
Anand khedkar <anandakhedkar23@gmail.com>
2024-08-26 01:07:11 +0530

    Add files via upload
    In this code we entered two integers as a input and we got addition,substraction, multiplication ,division and modules operator as our output

---
+++ Binary operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a = 6, b = 9;
+
+// Addition operator
+
+cout << "a + b = " << (a + b) << endl;
+
+// Subtraction operator
+cout << "a - b = " << (a - b) << endl;
+
+// Multiplication operator
+cout << "a * b = " << (a * b) << endl;
+
+// Division operator
+cout << "a / b = " << (a / b) << endl;
+
+// Modulo operator
+cout << "a % b = " << (a % b) << endl;
+
+return 0;
+}
