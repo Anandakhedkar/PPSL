51b7be4f0702441a81515c97446a7e650b6306f5
Anand khedkar <anandakhedkar23@gmail.com>
2024-08-26 01:18:11 +0530

    Add files via upload
    In this program we have to enter two numbers as our input and we will get larger number as our output.

---
+++ Program to demonstrate conditional operators .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int a=6,b=9;
+
+//Conditional operator
+int result=(a<b)?b:a;
+cout<<"The greatest number is"<<result<<endl;
+    return 0;
+}
